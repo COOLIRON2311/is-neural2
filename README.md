8c86687f8157eee7b96697e21c56d15b2a09c9fb
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2022-12-23 20:19:19 +0300

    Added files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# shuelab_aiml
+Уникальный токен для работы TGToken.txt (без него не заработает)
+Все изменения проводить в AIML файле aiml\russian.aiml
---
+++ neural/DataSet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using AIMLTGBot;
+
+namespace NeuralNetwork1
+{
+    public class DataSet
+    {
+
+        static Random rand = new Random();
+        private static Dictionary<string, FigureType> Classes = new Dictionary<string, FigureType>()
+        {
+            {"0", FigureType.Zero }, {"1", FigureType.One}, {"2", FigureType.Two}, {"3", FigureType.Three},
+            {"4", FigureType.Four}, {"5", FigureType.Five}, {"6", FigureType.Six}, {"7", FigureType.Seven},
+            {"8", FigureType.Eight}, {"9", FigureType.Nine}
+        };
+
+        public static SamplesSet GetDataSet(string path = "../../mydataset/")
+        {
+            SamplesSet s = new SamplesSet();
+            void LoadClass(string subdir)
+            {
+                foreach (var file in Directory.GetFiles(Path.Combine(path, subdir)))
+                {
+                    // if (rand.Next(0, 100) > 10) continue;
+                    Bitmap f = new Bitmap(file);
+                    s.AddSample(new Sample(ImageHelper.GetArray(f), 10, Classes[subdir]));
+                }
+            }
+
+            foreach (var subdir in Directory.GetDirectories(path))
+            {
+                LoadClass(Path.GetFileName(subdir));
+            }
+
+            s.samples = s.samples.OrderBy(i => rand.Next()).ToList();
+            
+            return s;
+        }
+    }
+}
